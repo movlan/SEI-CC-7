75c2d0446abee2f08ed8fcd7d382cb90fea3a68d
jim-clark <email@jim-clark.com>
2020-02-24 12:31:52 -0800

    Add consuming-3rd-party-api lesson

---
+++ work/w05/d2/03-consuming-3rd-party-apis/final-code/github-users/routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+const githubCtrl = require('../controllers/github');
+
+/* GET home page. */
+router.get('/', githubCtrl.userDetails);
+router.post('/', githubCtrl.userDetails);
+
+module.exports = router;
