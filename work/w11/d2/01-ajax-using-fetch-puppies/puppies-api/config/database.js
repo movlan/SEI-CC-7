dcbbd0f4478427cb8f4c4f4f449b3d3d67c5ef7f
Jim Haff <Jimmyhaff3@gmail.com>
2020-04-06 22:30:39 -0500

    Added ajax

---
+++ work/w11/d2/01-ajax-using-fetch-puppies/puppies-api/config/database.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+
+mongoose.connect(
+  process.env.DATABASE_URL,
+  { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+);
+
+var db = mongoose.connection;
+
+db.on('connected', function() {
+  console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
+});
---
+++ work/w11/d2/01-ajax-using-fetch-puppies/puppies-api/controllers/api/puppies.js
@@ -0,0 +1,49 @@
+const Puppy = require('../../models/puppy');
+
+module.exports = {
+  index,
+  show,
+  create,
+  delete: deleteOne,
+  update
+};
+
+function update(req, res) {
+  Puppy.findByIdAndUpdate(req.params.id, req.body, {new: true}).then(function(puppy) {
+    res.status(200).json(puppy);
+  }).catch(function(err) {
+    res.status(400).json(err);
+  });
+}
+
+function deleteOne(req, res) {
+  Puppy.findByIdAndDelete(req.params.id).then(function(puppy) {
+    res.status(200).json(puppy);
+  }).catch(function(err) {
+    res.status(400).json(err);
+  });
+}
+
+function show(req, res) {
+  Puppy.findById(req.params.id).then(function(puppy) {
+    res.status(200).json(puppy);
+  }).catch(function(err) {
+    res.status(400).json(err);
+  });
+}
+
+function create(req, res) {
+  Puppy.create(req.body).then(function(puppy) {
+    res.status(201).json(puppy);
+  }).catch(function(err) {
+    res.status(400).json(err);
+  });
+}
+
+function index(req, res) {
+  Puppy.find({}).then(function(puppies) {
+    res.status(200).json(puppies);
+  }).catch(function(err) {
+    res.status(400).json(err);
+  });
+}
